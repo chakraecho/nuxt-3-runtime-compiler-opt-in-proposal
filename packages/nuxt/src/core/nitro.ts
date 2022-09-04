45eac5f165b4c0e3b2dd3a766d1152d4ea6de370
julien huang <julien.huang@outlook.fr>
2022-09-04 10:36:41 +0200

    fix(nuxt): fix devtools stub due to merge

--- packages/nuxt/src/core/nitro.ts
+++ packages/nuxt/src/core/nitro.ts
@@ -87,7 +87,7 @@ export async function initNitro (nuxt: Nuxt) {
       '@vue/compiler-core': (nuxt.options.vue.runtimeCompiler && !nuxt.options.dev) ? '@vue/compiler-core' : 'unenv/runtime/mock/proxy',
       '@vue/compiler-dom': (nuxt.options.vue.runtimeCompiler && !nuxt.options.dev) ? '@vue/compiler-dom' : 'unenv/runtime/mock/proxy',
       '@vue/compiler-ssr': (nuxt.options.vue.runtimeCompiler && !nuxt.options.dev) ? '@vue/compiler-ssr' : 'unenv/runtime/mock/proxy',
-      '@vue/devtools-api': 'unenv/runtime/mock/proxy-cjs',
+      '@vue/devtools-api': 'vue-devtools-stub',
 
       // Paths
       '#paths': resolve(distDir, 'core/runtime/nitro/paths'),
